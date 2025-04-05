33c6570abbbd61427586a1239680959d57e25881
musfikanoni <musfikanoni@gmail.com>
2025-04-06 00:49:50 +0600

    created user apis

--- index.js
+++ index.js
@@ -26,6 +26,20 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
 
+    const userCollection = client.db("AURAFashionDB").collection("users");
+
+    // User API
+    app.post('/users', async(req, res) => {
+      const user = req.body;
+      const query = {email: user.email};
+      const existingUser = await userCollection.findOne(query);
+      if(existingUser){
+        return res.send({message: 'user already exist', insertedId: null})
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
     
 
     // await client.db("admin").command({ ping: 1 });
