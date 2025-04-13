dc1f6056b89278434657a8418a07e81d66676ccc
musfikanoni <musfikanoni@gmail.com>
2025-04-13 21:19:59 +0600

    created users get api

--- index.js
+++ index.js
@@ -29,6 +29,11 @@ async function run() {
     const userCollection = client.db("AURAFashionDB").collection("users");
 
     // User API
+    app.get('/users', async(req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
+    
     app.post('/users', async(req, res) => {
       const user = req.body;
       const query = {email: user.email};
