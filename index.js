52b1b6510d11248a82a7d60763fba9843f0c3dbd
musfikanoni <musfikanoni@gmail.com>
2025-04-13 22:24:03 +0600

    created user Delete opration

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -33,7 +33,7 @@ async function run() {
       const result = await userCollection.find().toArray();
       res.send(result);
     })
-    
+
     app.post('/users', async(req, res) => {
       const user = req.body;
       const query = {email: user.email};
@@ -42,6 +42,14 @@ async function run() {
         return res.send({message: 'user already exist', insertedId: null})
       }
       const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+    //user delete 
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query);
       res.send(result);
     })
 
